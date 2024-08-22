865048f3661af80170bf576fd0229bf340887da8
Mrunmayii <shirke.mrunmayi@gmail.com>
2024-08-22 09:18:06 +0530

    Updated 1.c

--- 1.c
+++ 1.c
@@ -1,7 +1,11 @@
+//Name: Mrunmayi Shirke
+//Roll No: MT2024144
+//Problem Statement: 1. Create the following types of a files using (i) shell command (ii) system call a). soft link (symlink system call)
+
 #include<stdio.h>
 #include<unistd.h>
 int main(){
-  symlink("filenew", "sftlnk");
+  symlink("newfile", "softlinknew");
   perror("output");
 }
 
