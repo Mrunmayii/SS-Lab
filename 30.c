ba6f091eea569a4b5348eddaff8c6a9d7c5be6a8
Mrunmayii <shirke.mrunmayi@gmail.com>
2024-08-31 12:57:00 +0530

    updated 30.c

--- 30.c
+++ 30.c
@@ -7,70 +7,38 @@ Date: 31st August, 2024
 ============================================================================
 */
 
-
-#include <stdio.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <stdlib.h>
-#include <sys/time.h>
-
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/stat.h>
+#include<stdlib.h>
 
 int main(){
-	int inp;
-	printf("Enter the number of nimutes after which you want to run the task:");
-        scanf("%d",&inp);
-
-        if(!fork()){
-                setsid();
-                chdir("/");
-                //umask(0);
-
-		printf("Inside daemon process\n");
-		struct timeval now;
-    		int rc;
-		
-    		rc=gettimeofday(&now, NULL);	
-    		
-		if(rc==0) {
-        		printf("gettimeofday() successful.\n");
-        		printf("current time in seconds.milliseconds = %lu.%06lu\n",now.tv_sec, now.tv_usec);
-    		}
-    		else {
-        		printf("gettimeofday() failed\n");
-        		return -1;
-    		}
-		
-		//scanf("%d",&inp);
-
-		int newtime = now.tv_sec + inp*60;
-
-		while(newtime!=now.tv_sec){			
-			rc = gettimeofday(&now, NULL);
-
-			if(newtime==now.tv_sec){
-				printf("Task executed successfully after %d minutes\n", inp);
-				printf("After executing the task current time is: %lu\n",now.tv_sec);
+	if (!fork()){
+		setsid();
+		chdir("/");
+		umask(0);
+		while(1){
+			sleep(5);
+			printf("Child pid:%d\n", getpid());
+			printf("daemon is running");
 			}
 		}
-
-	}
-        
-	else
-                exit(0);
-	printf("outside daemon\n");
-        return 0;
+		else{
+		exit(0);
+		}
 }
-
 
 /*
 OUTPUT:
+mrunmayi@mrunmayi-HP-Laptop-14s-dr1xxx:~/SS-Lab$ vim 30.c
+mrunmayi@mrunmayi-HP-Laptop-14s-dr1xxx:~/SS-Lab$ gcc 30.c
 mrunmayi@mrunmayi-HP-Laptop-14s-dr1xxx:~/SS-Lab$ ./a.out
-Enter the number of nimutes after which you want to run the task:1
-Inside daemon process
-gettimeofday() successful.
-current time in seconds.milliseconds = 1725085307.632751
-mrunmayi@mrunmayi-HP-Laptop-14s-dr1xxx:~/SS-Lab$ Task executed successfully after 1 minutes
-After executing the task current time is: 1725085367
-outside daemon
+mrunmayi@mrunmayi-HP-Laptop-14s-dr1xxx:~/SS-Lab$ Child pid:4805
+pdaemon is runningChild pid:4805
+pdaemon is runningChild pid:4805
+pdaemon is runningChild pid:4805
+
+mrunmayi@mrunmayi-HP-Laptop-14s-dr1xxx:~/SS-Lab$ ps -el| grep 4805
+1 S  1000    4805    1383  0  80   0 -   654 hrtime ?        00:00:00 a.out
+mrunmayi@mrunmayi-HP-Laptop-14s-dr1xxx:~/SS-Lab$ daemon is runningChild pid:4805
 */
